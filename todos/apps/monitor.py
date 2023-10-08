ff50aa4c406b89ccd08e5f7e3c3739930b7182de
Eunseo Kim <eunseo.thatsme@gmail.com>
2023-10-08 23:55:12 +0900

    move: todos to /src/todos

--- todos/apps/monitor.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
-
-app = FastAPI()
-
-
-@app.get("/l7check", tags=["health check"], summary="Server health check")
-def health_check() -> JSONResponse:
-    return JSONResponse(content={"status": "ok"})
