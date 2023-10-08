3a29f4ce195d13d61cdd9797a128eb4b34116b20
Eunseo Kim <eunseo.thatsme@gmail.com>
2023-10-09 00:51:50 +0900

    refactor: fix API title and description

--- src/todos/main.py
+++ src/todos/main.py
@@ -7,8 +7,8 @@ from src.todos.apps.monitor import app as app_monitor
 from src.todos.apps.v1.todos import app as app_todos_v1
 
 app = FastAPI(
-    title="Grid Maker",
-    description="It is a simple API which can create a grid of images.",
+    title="My Todo List API",
+    description="It is a simple TODO CRUD API.",
 )
 app.mount("/monitor", app_monitor)
 app.mount("/todos/v1", app_todos_v1)
@@ -22,7 +22,7 @@ def apis(request: Request) -> list[str]:
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Grid Maker")
+    parser = argparse.ArgumentParser(description="My Todo List API")
     parser.add_argument("--host", type=str, default="0.0.0.0", help="Host IP")
     parser.add_argument("--port", type=int, default=8000, help="Port")
     args = parser.parse_args()
