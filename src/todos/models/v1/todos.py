49c4f5fd7219a8b13d2d4641a46792a9d7dd955a
Eunseo Kim <eunseo.thatsme@gmail.com>
2023-10-09 20:42:55 +0900

    refactor: change pydantic model

--- src/todos/models/v1/todos.py
+++ src/todos/models/v1/todos.py
@@ -4,13 +4,16 @@ from pydantic import BaseModel, Field
 
 
 class TodoItem(BaseModel):
-    id: int = Field(default=-1, title="ID")
     title: str = Field(..., title="Title", max_length=100)
     description: str = Field(..., title="Description", max_length=100)
     completed: bool = Field(False, title="Completed")
+
+
+class TodoMetadata(TodoItem):
+    id: str = Field(..., title="ID")
     created_at: datetime.datetime = Field(None, title="Created At")
     updated_at: datetime.datetime = Field(None, title="Updated At")
 
 
-class TodoRetrieve(TodoItem):
-    todo_items: list[TodoItem]
+class RetreiveResponse(BaseModel):
+    todo_list: list[TodoMetadata] = Field(default=[], title="TODO items")
