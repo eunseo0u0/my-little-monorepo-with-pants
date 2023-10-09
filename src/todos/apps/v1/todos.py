9a810b3d48aba6640c1f7169f0d6cad8624dd268
Eunseo Kim <eunseo.thatsme@gmail.com>
2023-10-09 20:43:36 +0900

    refactor: change interface

--- src/todos/apps/v1/todos.py
+++ src/todos/apps/v1/todos.py
@@ -1,13 +1,18 @@
 import datetime
+from uuid import uuid4
 
 from fastapi import Body, FastAPI
 from fastapi.responses import JSONResponse
 
-from src.todos.models.v1.todos import RetreiveResponse, TodoItem
+from src.todos.models.v1.todos import (
+    RetreiveResponse,
+    TodoItem,
+    TodoMetadata,
+)
 
 app = FastAPI()
 
-todo_list: list[TodoItem] = []
+todo_list: list[TodoMetadata] = []
 
 
 @app.post("/add", tags=["add-todo"], summary="Add TODO item.")
@@ -21,15 +26,20 @@ async def add_todo_item(
         },
     )
 ) -> JSONResponse:
-    todo_item.id = len(todo_list)
-    todo_item.created_at = datetime.datetime.utcnow()
-    todo_list.append(todo_item)
+    todo_metadata: TodoMetadata = TodoMetadata(
+        id=uuid4().hex,
+        title=todo_item.title,
+        description=todo_item.description,
+        completed=todo_item.completed,
+        created_at=datetime.datetime.utcnow(),
+    )
+    todo_list.append(todo_metadata)
     return JSONResponse(status_code=200, content={"status": "Add item successfully."})
 
 
 @app.put("/update/{todo_id}", tags=["update-todo"], summary="Update TODO item.")
 async def update_todo_item(
-    todo_id: int,
+    todo_id: str,
     todo_item: TodoItem = Body(
         ...,
         examples={
@@ -53,11 +63,12 @@ async def update_todo_item(
 
 @app.get(
     "/retrieve",
+    response_model=RetreiveResponse,
     tags=["retrieve-todo"],
     summary="Retrieve TODO items.",
 )
-async def retrieve_todo_items() -> dict:
-    return {"todo_items": todo_list}
+async def retrieve_todo_items() -> RetreiveResponse:
+    return RetreiveResponse(todo_list=todo_list)
 
 
 @app.delete(
@@ -65,7 +76,7 @@ async def retrieve_todo_items() -> dict:
     tags=["delete-todo"],
     summary="Delete TODO item.",
 )
-async def delete_todo_item(todo_id: int) -> JSONResponse:
+async def delete_todo_item(todo_id: str) -> JSONResponse:
     for i, todo in enumerate(todo_list):
         if todo.id == todo_id:
             todo_list.pop(i)
