2193c5e46d62c35f2236e42bf2c5e81c11944d16
Eunseo Kim <eunseo.thatsme@gmail.com>
2023-10-29 20:44:57 +0900

    refactor: change db type

--- src/todos/apps/v1/todos.py
+++ src/todos/apps/v1/todos.py
@@ -12,7 +12,7 @@ from src.todos.models.v1.todos import (
 
 app = FastAPI()
 
-todo_list: list[TodoMetadata] = []
+todo_db: dict[str, TodoMetadata] = {}
 
 
 @app.post("/add", tags=["add-todo"], summary="Add TODO item.")
@@ -35,22 +35,22 @@ async def add_todo_item(
         JSONResponse with a status code and a detailed message.
     """
     todo_metadata: TodoMetadata = TodoMetadata(
-        id=uuid4().hex,
         title=request.title,
         description=request.description,
         completed=request.completed,
         created_at=datetime.datetime.utcnow(),
     )
-    todo_list.append(todo_metadata)
+    item_id: str = uuid4().hex
+    todo_db[item_id] = todo_metadata
     return JSONResponse(
         status_code=200,
-        content={"detail": f"Add item: '{todo_metadata.id}' successfully."},
+        content={"detail": f"Add item: '{item_id}' successfully."},
     )
 
 
-@app.put("/update/{todo_id}", tags=["update-todo"], summary="Update TODO item.")
+@app.put("/update/{item_id}", tags=["update-todo"], summary="Update TODO item.")
 async def update_todo_item(
-    todo_id: str,
+    item_id: str,
     request: TodoRequest = Body(
         ...,
         example={
@@ -63,23 +63,26 @@ async def update_todo_item(
     """Update todo item.
 
     Args:
-        todo_id: An ID of todo item to be updated.
+        item_id: An ID of todo item to be updated.
         request: Todo item request including title, description and completed.
 
     Returns:
         JSONResponse with a status code and a detailed message.
     """
-    for i, todo in enumerate(todo_list):
-        if todo.id == todo_id:
-            todo.title = request.title
-            todo.description = request.description
-            todo.completed = request.completed
-            todo.updated_at = datetime.datetime.utcnow()
-            todo_list[i] = todo
-            return JSONResponse(
-                status_code=200,
-                content={"detail": f"Update item: '{todo_id}' successfully."},
-            )
+    try:
+        todo_db[item_id].title = request.title
+        todo_db[item_id].description = request.description
+        todo_db[item_id].completed = request.completed
+        todo_db[item_id].updated_at = datetime.datetime.utcnow()
+        return JSONResponse(
+            status_code=200,
+            content={"detail": f"Update item: '{item_id}' successfully."},
+        )
+    except KeyError:
+        return JSONResponse(
+            status_code=404,
+            content={"detail": f"Item: '{item_id}' not found."},
+        )
 
 
 @app.get(
@@ -94,7 +97,7 @@ async def retrieve_todo_items() -> RetreiveResponse:
     Returns:
         RetreiveResponse with a list of todo items.
     """
-    return RetreiveResponse(todo_list=todo_list)
+    return RetreiveResponse(todo_items=todo_db)
 
 
 @app.delete(
@@ -102,19 +105,23 @@ async def retrieve_todo_items() -> RetreiveResponse:
     tags=["delete-todo"],
     summary="Delete TODO item.",
 )
-async def delete_todo_item(todo_id: str) -> JSONResponse:
+async def delete_todo_item(item_id: str) -> JSONResponse:
     """Delete todo item.
 
     Args:
-        todo_id: An ID of todo item to be deleted.
+        item_id: An ID of todo item to be deleted.
 
     Returns:
         JSONResponse with a status code and a detailed message.
     """
-    for i, todo in enumerate(todo_list):
-        if todo.id == todo_id:
-            todo_list.pop(i)
-            return JSONResponse(
-                status_code=200,
-                content={"detail": f"Delete item: '{todo_id}' successfully."},
-            )
+    try:
+        todo_db.pop(item_id)
+        return JSONResponse(
+            status_code=200,
+            content={"detail": f"Update item: '{item_id}' successfully."},
+        )
+    except KeyError:
+        return JSONResponse(
+            status_code=404,
+            content={"detail": f"Item: '{item_id}' not found."},
+        )
--- src/todos/models/v1/todos.py
+++ src/todos/models/v1/todos.py
@@ -6,7 +6,6 @@ from pydantic import BaseModel, Field
 class TodoMetadata(BaseModel):
     """Todo metadata."""
 
-    id: str = Field(..., title="ID")
     title: str = Field(..., title="Title", max_length=100)
     description: str = Field(..., title="Description", max_length=100)
     completed: bool = Field(default=False, title="Completed")
@@ -25,4 +24,4 @@ class TodoRequest(BaseModel):
 class RetreiveResponse(BaseModel):
     """Retrieve response model used in /retrieve."""
 
-    todo_list: list[TodoMetadata] = Field(default=[], title="TODO items")
+    todo_items: dict[str, TodoMetadata] = Field(default={}, title="TODO items")
