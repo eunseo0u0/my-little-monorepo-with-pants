dcd88ba061def551d59278d7bbd362029f1bb1c0
Eunseo Kim <eunseo.thatsme@gmail.com>
2023-10-29 20:54:31 +0900

    refactor: change TodoMetadata default value

--- src/todos/models/v1/todos.py
+++ src/todos/models/v1/todos.py
@@ -6,8 +6,8 @@ from pydantic import BaseModel, Field
 class TodoMetadata(BaseModel):
     """Todo metadata."""
 
-    title: str = Field(..., title="Title", max_length=100)
-    description: str = Field(..., title="Description", max_length=100)
+    title: str = Field(default="", title="Title", max_length=100)
+    description: str = Field(default="", title="Description", max_length=100)
     completed: bool = Field(default=False, title="Completed")
     created_at: datetime.datetime = Field(..., title="Created At")
     updated_at: datetime.datetime = Field(default=None, title="Updated At")
