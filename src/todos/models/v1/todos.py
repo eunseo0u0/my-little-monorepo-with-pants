6161772e2925626e456d9ed07e87c4f692cf80ba
Eunseo Kim <eunseo.thatsme@gmail.com>
2023-10-29 18:29:08 +0900

    refactor: change models

--- src/todos/models/v1/todos.py
+++ src/todos/models/v1/todos.py
@@ -3,17 +3,26 @@ import datetime
 from pydantic import BaseModel, Field
 
 
-class TodoItem(BaseModel):
+class TodoMetadata(BaseModel):
+    """Todo metadata."""
+
+    id: str = Field(..., title="ID")
     title: str = Field(..., title="Title", max_length=100)
     description: str = Field(..., title="Description", max_length=100)
-    completed: bool = Field(False, title="Completed")
+    completed: bool = Field(default=False, title="Completed")
+    created_at: datetime.datetime = Field(..., title="Created At")
+    updated_at: datetime.datetime = Field(default=None, title="Updated At")
 
 
-class TodoMetadata(TodoItem):
-    id: str = Field(..., title="ID")
-    created_at: datetime.datetime = Field(None, title="Created At")
-    updated_at: datetime.datetime = Field(None, title="Updated At")
+class TodoRequest(BaseModel):
+    """Todo request model used in /add and /update."""
+
+    title: str = Field(..., title="Title", max_length=100)
+    description: str = Field(..., title="Description", max_length=100)
+    completed: bool = Field(default=False, title="Completed")
 
 
 class RetreiveResponse(BaseModel):
+    """Retrieve response model used in /retrieve."""
+
     todo_list: list[TodoMetadata] = Field(default=[], title="TODO items")
