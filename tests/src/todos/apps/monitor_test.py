7b61706cc8fa1abeb7efde9e1f88bb824ce7db56
Eunseo Kim <eunseo.thatsme@gmail.com>
2023-11-19 20:11:53 +0900

    refactor: add docstring

--- tests/src/todos/apps/monitor_test.py
+++ tests/src/todos/apps/monitor_test.py
@@ -7,5 +7,6 @@ class TestMonitorAPI:
     client = TestClient(app)
 
     def test_health_check_api(self) -> None:
+        """Test server health check."""
         response = self.client.get(url="/l7check")
         assert response.status_code == 200
